7db34fe5311980120da14169e693c5c5504ce2aa
tsou <40441140@gm.nfu.edu.tw>
2020-04-08 11:58:19 +0800

    discount

--- WindowsFormsApp1/Order_From.Designer.cs
+++ WindowsFormsApp1/Order_From.Designer.cs
@@ -23,57 +23,34 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.total_title_label = new System.Windows.Forms.Label();
+            this.total_lebel = new System.Windows.Forms.Label();
+            this.shopping_list_label = new System.Windows.Forms.Label();
+            this.list_label = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.total_title_label = new System.Windows.Forms.Label();
-            this.total_lebel = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
-            this.shopping_list_label = new System.Windows.Forms.Label();
-            this.list_label = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.guest_radioButton = new System.Windows.Forms.RadioButton();
+            this.VIP_radioButton = new System.Windows.Forms.RadioButton();
+            this.discount_label = new System.Windows.Forms.Label();
+            this.discount_title_label = new System.Windows.Forms.Label();
+            this.pay_label = new System.Windows.Forms.Label();
+            this.pay_title_label = new System.Windows.Forms.Label();
+            this.production_listBox = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(306, 49);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 92);
-            this.button1.TabIndex = 0;
-            this.button1.Tag = "50";
-            this.button1.Text = "奶茶($50)";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button2.Location = new System.Drawing.Point(602, 49);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(159, 92);
-            this.button2.TabIndex = 1;
-            this.button2.Tag = "30";
-            this.button2.Text = "紅茶($30)";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button3.Location = new System.Drawing.Point(870, 49);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(159, 92);
-            this.button3.TabIndex = 2;
-            this.button3.Tag = "70";
-            this.button3.Text = "嘎逼($70)";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // total_title_label
             // 
             this.total_title_label.AutoSize = true;
             this.total_title_label.Font = new System.Drawing.Font("PMingLiU", 16F);
-            this.total_title_label.Location = new System.Drawing.Point(39, 314);
+            this.total_title_label.Location = new System.Drawing.Point(866, 279);
             this.total_title_label.Name = "total_title_label";
             this.total_title_label.Size = new System.Drawing.Size(93, 27);
             this.total_title_label.TabIndex = 3;
@@ -83,22 +60,11 @@
             // 
             this.total_lebel.AutoSize = true;
             this.total_lebel.Font = new System.Drawing.Font("PMingLiU", 16F);
-            this.total_lebel.Location = new System.Drawing.Point(138, 314);
+            this.total_lebel.Location = new System.Drawing.Point(965, 279);
             this.total_lebel.Name = "total_lebel";
             this.total_lebel.Size = new System.Drawing.Size(25, 27);
             this.total_lebel.TabIndex = 4;
             this.total_lebel.Text = "0";
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button4.Location = new System.Drawing.Point(870, 249);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(159, 92);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "清除";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // shopping_list_label
             // 
@@ -119,11 +85,195 @@
             this.list_label.Size = new System.Drawing.Size(0, 27);
             this.list_label.TabIndex = 7;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(268, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "50";
+            this.button1.Text = "奶茶($50)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(451, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 92);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "30";
+            this.button2.Text = "紅茶($30)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(632, 152);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 92);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "70";
+            this.button3.Text = "嘎逼($70)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(871, 152);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 92);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "清除";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(537, 31);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 23);
+            this.comboBox.TabIndex = 8;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.guest_radioButton);
+            this.groupBox.Controls.Add(this.VIP_radioButton);
+            this.groupBox.Location = new System.Drawing.Point(268, 12);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(236, 119);
+            this.groupBox.TabIndex = 9;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "身分識別";
+            // 
+            // guest_radioButton
+            // 
+            this.guest_radioButton.AutoSize = true;
+            this.guest_radioButton.Location = new System.Drawing.Point(7, 77);
+            this.guest_radioButton.Name = "guest_radioButton";
+            this.guest_radioButton.Size = new System.Drawing.Size(73, 19);
+            this.guest_radioButton.TabIndex = 1;
+            this.guest_radioButton.TabStop = true;
+            this.guest_radioButton.Text = "非會員";
+            this.guest_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // VIP_radioButton
+            // 
+            this.VIP_radioButton.AutoSize = true;
+            this.VIP_radioButton.Location = new System.Drawing.Point(7, 37);
+            this.VIP_radioButton.Name = "VIP_radioButton";
+            this.VIP_radioButton.Size = new System.Drawing.Size(128, 19);
+            this.VIP_radioButton.TabIndex = 0;
+            this.VIP_radioButton.TabStop = true;
+            this.VIP_radioButton.Text = "VIP會員(打9折)";
+            this.VIP_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // discount_label
+            // 
+            this.discount_label.AutoSize = true;
+            this.discount_label.Font = new System.Drawing.Font("PMingLiU", 16F);
+            this.discount_label.Location = new System.Drawing.Point(965, 335);
+            this.discount_label.Name = "discount_label";
+            this.discount_label.Size = new System.Drawing.Size(25, 27);
+            this.discount_label.TabIndex = 11;
+            this.discount_label.Text = "0";
+            // 
+            // discount_title_label
+            // 
+            this.discount_title_label.AutoSize = true;
+            this.discount_title_label.Font = new System.Drawing.Font("PMingLiU", 16F);
+            this.discount_title_label.Location = new System.Drawing.Point(866, 335);
+            this.discount_title_label.Name = "discount_title_label";
+            this.discount_title_label.Size = new System.Drawing.Size(93, 27);
+            this.discount_title_label.TabIndex = 10;
+            this.discount_title_label.Text = "折扣：";
+            // 
+            // pay_label
+            // 
+            this.pay_label.AutoSize = true;
+            this.pay_label.Font = new System.Drawing.Font("PMingLiU", 16F);
+            this.pay_label.Location = new System.Drawing.Point(965, 392);
+            this.pay_label.Name = "pay_label";
+            this.pay_label.Size = new System.Drawing.Size(25, 27);
+            this.pay_label.TabIndex = 13;
+            this.pay_label.Text = "0";
+            // 
+            // pay_title_label
+            // 
+            this.pay_title_label.AutoSize = true;
+            this.pay_title_label.Font = new System.Drawing.Font("PMingLiU", 16F);
+            this.pay_title_label.Location = new System.Drawing.Point(866, 392);
+            this.pay_title_label.Name = "pay_title_label";
+            this.pay_title_label.Size = new System.Drawing.Size(93, 27);
+            this.pay_title_label.TabIndex = 12;
+            this.pay_title_label.Text = "應付：";
+            // 
+            // production_listBox
+            // 
+            this.production_listBox.FormattingEnabled = true;
+            this.production_listBox.ItemHeight = 15;
+            this.production_listBox.Location = new System.Drawing.Point(268, 469);
+            this.production_listBox.Name = "production_listBox";
+            this.production_listBox.Size = new System.Drawing.Size(523, 94);
+            this.production_listBox.TabIndex = 14;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.Location = new System.Drawing.Point(268, 279);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 92);
+            this.button5.TabIndex = 15;
+            this.button5.Tag = "10";
+            this.button5.Text = "綠茶($10)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button6.Location = new System.Drawing.Point(451, 279);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(159, 92);
+            this.button6.TabIndex = 16;
+            this.button6.Tag = "20";
+            this.button6.Text = "可樂($20)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button7.Location = new System.Drawing.Point(632, 279);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(159, 92);
+            this.button7.TabIndex = 17;
+            this.button7.Tag = "40";
+            this.button7.Text = "果汁($40)";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // Order_From
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1066, 450);
+            this.ClientSize = new System.Drawing.Size(1066, 646);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.production_listBox);
+            this.Controls.Add(this.pay_label);
+            this.Controls.Add(this.pay_title_label);
+            this.Controls.Add(this.discount_label);
+            this.Controls.Add(this.discount_title_label);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.comboBox);
             this.Controls.Add(this.list_label);
             this.Controls.Add(this.shopping_list_label);
             this.Controls.Add(this.button4);
@@ -135,20 +285,33 @@
             this.Name = "Order_From";
             this.Text = "Order_From";
             this.Load += new System.EventHandler(this.Order_From_Load);
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Label total_title_label;
+        private System.Windows.Forms.Label total_lebel;
+        private System.Windows.Forms.Label shopping_list_label;
+        private System.Windows.Forms.Label list_label;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label total_title_label;
-        private System.Windows.Forms.Label total_lebel;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label shopping_list_label;
-        private System.Windows.Forms.Label list_label;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.RadioButton guest_radioButton;
+        private System.Windows.Forms.RadioButton VIP_radioButton;
+        private System.Windows.Forms.Label discount_label;
+        private System.Windows.Forms.Label discount_title_label;
+        private System.Windows.Forms.Label pay_label;
+        private System.Windows.Forms.Label pay_title_label;
+        private System.Windows.Forms.ListBox production_listBox;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
--- WindowsFormsApp1/Order_From.cs
+++ WindowsFormsApp1/Order_From.cs
@@ -11,16 +11,20 @@ using System.Windows.Forms;
 namespace WindowsFormsApp1 {
     public partial class Order_From : Form {
         int total = 0;
-        int milk_tea = 0;
-        int black_tea = 0;
-        int coffee = 0;
+        int buy_num = 1;
+        //綠茶按鈕10快 可樂20 果之40
+   
 
         public Order_From() {
             InitializeComponent();
         }
 
         private void Order_From_Load(object sender, EventArgs e) {
-
+            guest_radioButton.Checked = true;
+            for (int i=1; i<=500; i++){
+                comboBox.Items.Add(i);
+            }
+            comboBox.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e) {
@@ -37,20 +41,50 @@ namespace WindowsFormsApp1 {
 
         private void button4_Click(object sender, EventArgs e) {
             total = 0;
-            milk_tea = 0;
-            black_tea = 0;
-            coffee = 0;
-            total_lebel.Text = total.ToString();
-            list_label.Text = "";
+            //total_lebel.Text = total.ToString();
+            //list_label.Text = "";
+            production_listBox.Items.Clear();
+            cal_pay();
         }
 
         public void cal(object sender) {
             //Button b = (Button)sender;
             Button b = sender as Button;
             total += Convert.ToInt32(b.Tag);
-            total_lebel.Text = total.ToString();
-            list_label.Text += b.Text + "\r\n";
+            //total_lebel.Text = total.ToString();
+            //list_label.Text += b.Text + "\r\n";
+            production_listBox.Items.Add(b.Text);
+            cal_pay();
+        }
 
+        public void cal_pay() {
+            double pay = 0;
+            double discount = 0;
+
+            if (VIP_radioButton.Checked) {
+                discount = total * 0.1;
+            }
+            else if (guest_radioButton.Checked) {
+                discount = 0;
+            }
+            pay = total - discount;
+            discount_label.Text = discount.ToString();
+            pay_label.Text = pay.ToString();
+        }
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e) {
+            buy_num = Convert.ToInt32(comboBox.SelectedItem.ToString());
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            cal(sender);
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+            cal(sender);
+        }
+
+        private void button7_Click(object sender, EventArgs e) {
+            cal(sender);
         }
     }
 }
