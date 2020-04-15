80fe37d4948a0586fa5870193290ac78bdcdd835
TsouMinHan <p87089905@gmail.com>
2020-04-15 12:01:22 +0800

    Add files via upload

--- WindowsFormsApp1/Order_From.Designer.cs
+++ WindowsFormsApp1/Order_From.Designer.cs
@@ -43,54 +43,61 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // total_title_label
             // 
             this.total_title_label.AutoSize = true;
-            this.total_title_label.Font = new System.Drawing.Font("PMingLiU", 16F);
-            this.total_title_label.Location = new System.Drawing.Point(866, 279);
+            this.total_title_label.Font = new System.Drawing.Font("新細明體", 16F);
+            this.total_title_label.Location = new System.Drawing.Point(849, 230);
+            this.total_title_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.total_title_label.Name = "total_title_label";
-            this.total_title_label.Size = new System.Drawing.Size(93, 27);
+            this.total_title_label.Size = new System.Drawing.Size(76, 22);
             this.total_title_label.TabIndex = 3;
             this.total_title_label.Text = "總價：";
             // 
             // total_lebel
             // 
             this.total_lebel.AutoSize = true;
-            this.total_lebel.Font = new System.Drawing.Font("PMingLiU", 16F);
-            this.total_lebel.Location = new System.Drawing.Point(965, 279);
+            this.total_lebel.Font = new System.Drawing.Font("新細明體", 16F);
+            this.total_lebel.Location = new System.Drawing.Point(923, 230);
+            this.total_lebel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.total_lebel.Name = "total_lebel";
-            this.total_lebel.Size = new System.Drawing.Size(25, 27);
+            this.total_lebel.Size = new System.Drawing.Size(20, 22);
             this.total_lebel.TabIndex = 4;
             this.total_lebel.Text = "0";
             // 
             // shopping_list_label
             // 
             this.shopping_list_label.AutoSize = true;
-            this.shopping_list_label.Font = new System.Drawing.Font("PMingLiU", 16F);
-            this.shopping_list_label.Location = new System.Drawing.Point(39, 49);
+            this.shopping_list_label.Font = new System.Drawing.Font("新細明體", 16F);
+            this.shopping_list_label.Location = new System.Drawing.Point(29, 39);
+            this.shopping_list_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.shopping_list_label.Name = "shopping_list_label";
-            this.shopping_list_label.Size = new System.Drawing.Size(147, 27);
+            this.shopping_list_label.Size = new System.Drawing.Size(120, 22);
             this.shopping_list_label.TabIndex = 6;
             this.shopping_list_label.Text = "購買清單：";
             // 
             // list_label
             // 
             this.list_label.AutoSize = true;
-            this.list_label.Font = new System.Drawing.Font("PMingLiU", 16F);
-            this.list_label.Location = new System.Drawing.Point(39, 98);
+            this.list_label.Font = new System.Drawing.Font("新細明體", 16F);
+            this.list_label.Location = new System.Drawing.Point(29, 78);
+            this.list_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.list_label.Name = "list_label";
-            this.list_label.Size = new System.Drawing.Size(0, 27);
+            this.list_label.Size = new System.Drawing.Size(0, 22);
             this.list_label.TabIndex = 7;
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(268, 152);
+            this.button1.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(400, 129);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 92);
+            this.button1.Size = new System.Drawing.Size(119, 74);
             this.button1.TabIndex = 0;
             this.button1.Tag = "50";
             this.button1.Text = "奶茶($50)";
@@ -99,10 +106,11 @@
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button2.Location = new System.Drawing.Point(451, 152);
+            this.button2.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(537, 129);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(159, 92);
+            this.button2.Size = new System.Drawing.Size(119, 74);
             this.button2.TabIndex = 1;
             this.button2.Tag = "30";
             this.button2.Text = "紅茶($30)";
@@ -111,10 +119,11 @@
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button3.Location = new System.Drawing.Point(632, 152);
+            this.button3.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(673, 129);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(159, 92);
+            this.button3.Size = new System.Drawing.Size(119, 74);
             this.button3.TabIndex = 2;
             this.button3.Tag = "70";
             this.button3.Text = "嘎逼($70)";
@@ -123,10 +132,11 @@
             // 
             // button4
             // 
-            this.button4.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button4.Location = new System.Drawing.Point(871, 152);
+            this.button4.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(852, 129);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(159, 92);
+            this.button4.Size = new System.Drawing.Size(119, 74);
             this.button4.TabIndex = 5;
             this.button4.Text = "清除";
             this.button4.UseVisualStyleBackColor = true;
@@ -135,9 +145,10 @@
             // comboBox
             // 
             this.comboBox.FormattingEnabled = true;
-            this.comboBox.Location = new System.Drawing.Point(537, 31);
+            this.comboBox.Location = new System.Drawing.Point(602, 32);
+            this.comboBox.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox.Name = "comboBox";
-            this.comboBox.Size = new System.Drawing.Size(121, 23);
+            this.comboBox.Size = new System.Drawing.Size(92, 20);
             this.comboBox.TabIndex = 8;
             this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
             // 
@@ -145,9 +156,11 @@
             // 
             this.groupBox.Controls.Add(this.guest_radioButton);
             this.groupBox.Controls.Add(this.VIP_radioButton);
-            this.groupBox.Location = new System.Drawing.Point(268, 12);
+            this.groupBox.Location = new System.Drawing.Point(400, 17);
+            this.groupBox.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox.Name = "groupBox";
-            this.groupBox.Size = new System.Drawing.Size(236, 119);
+            this.groupBox.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox.Size = new System.Drawing.Size(177, 95);
             this.groupBox.TabIndex = 9;
             this.groupBox.TabStop = false;
             this.groupBox.Text = "身分識別";
@@ -155,9 +168,10 @@
             // guest_radioButton
             // 
             this.guest_radioButton.AutoSize = true;
-            this.guest_radioButton.Location = new System.Drawing.Point(7, 77);
+            this.guest_radioButton.Location = new System.Drawing.Point(5, 62);
+            this.guest_radioButton.Margin = new System.Windows.Forms.Padding(2);
             this.guest_radioButton.Name = "guest_radioButton";
-            this.guest_radioButton.Size = new System.Drawing.Size(73, 19);
+            this.guest_radioButton.Size = new System.Drawing.Size(59, 16);
             this.guest_radioButton.TabIndex = 1;
             this.guest_radioButton.TabStop = true;
             this.guest_radioButton.Text = "非會員";
@@ -167,9 +181,10 @@
             // VIP_radioButton
             // 
             this.VIP_radioButton.AutoSize = true;
-            this.VIP_radioButton.Location = new System.Drawing.Point(7, 37);
+            this.VIP_radioButton.Location = new System.Drawing.Point(5, 30);
+            this.VIP_radioButton.Margin = new System.Windows.Forms.Padding(2);
             this.VIP_radioButton.Name = "VIP_radioButton";
-            this.VIP_radioButton.Size = new System.Drawing.Size(128, 19);
+            this.VIP_radioButton.Size = new System.Drawing.Size(103, 16);
             this.VIP_radioButton.TabIndex = 0;
             this.VIP_radioButton.TabStop = true;
             this.VIP_radioButton.Text = "VIP會員(打9折)";
@@ -179,58 +194,64 @@
             // discount_label
             // 
             this.discount_label.AutoSize = true;
-            this.discount_label.Font = new System.Drawing.Font("PMingLiU", 16F);
-            this.discount_label.Location = new System.Drawing.Point(965, 335);
+            this.discount_label.Font = new System.Drawing.Font("新細明體", 16F);
+            this.discount_label.Location = new System.Drawing.Point(923, 275);
+            this.discount_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.discount_label.Name = "discount_label";
-            this.discount_label.Size = new System.Drawing.Size(25, 27);
+            this.discount_label.Size = new System.Drawing.Size(20, 22);
             this.discount_label.TabIndex = 11;
             this.discount_label.Text = "0";
             // 
             // discount_title_label
             // 
             this.discount_title_label.AutoSize = true;
-            this.discount_title_label.Font = new System.Drawing.Font("PMingLiU", 16F);
-            this.discount_title_label.Location = new System.Drawing.Point(866, 335);
+            this.discount_title_label.Font = new System.Drawing.Font("新細明體", 16F);
+            this.discount_title_label.Location = new System.Drawing.Point(849, 275);
+            this.discount_title_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.discount_title_label.Name = "discount_title_label";
-            this.discount_title_label.Size = new System.Drawing.Size(93, 27);
+            this.discount_title_label.Size = new System.Drawing.Size(76, 22);
             this.discount_title_label.TabIndex = 10;
             this.discount_title_label.Text = "折扣：";
             // 
             // pay_label
             // 
             this.pay_label.AutoSize = true;
-            this.pay_label.Font = new System.Drawing.Font("PMingLiU", 16F);
-            this.pay_label.Location = new System.Drawing.Point(965, 392);
+            this.pay_label.Font = new System.Drawing.Font("新細明體", 16F);
+            this.pay_label.Location = new System.Drawing.Point(923, 321);
+            this.pay_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.pay_label.Name = "pay_label";
-            this.pay_label.Size = new System.Drawing.Size(25, 27);
+            this.pay_label.Size = new System.Drawing.Size(20, 22);
             this.pay_label.TabIndex = 13;
             this.pay_label.Text = "0";
             // 
             // pay_title_label
             // 
             this.pay_title_label.AutoSize = true;
-            this.pay_title_label.Font = new System.Drawing.Font("PMingLiU", 16F);
-            this.pay_title_label.Location = new System.Drawing.Point(866, 392);
+            this.pay_title_label.Font = new System.Drawing.Font("新細明體", 16F);
+            this.pay_title_label.Location = new System.Drawing.Point(849, 321);
+            this.pay_title_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.pay_title_label.Name = "pay_title_label";
-            this.pay_title_label.Size = new System.Drawing.Size(93, 27);
+            this.pay_title_label.Size = new System.Drawing.Size(76, 22);
             this.pay_title_label.TabIndex = 12;
             this.pay_title_label.Text = "應付：";
             // 
             // production_listBox
             // 
             this.production_listBox.FormattingEnabled = true;
-            this.production_listBox.ItemHeight = 15;
-            this.production_listBox.Location = new System.Drawing.Point(268, 469);
+            this.production_listBox.ItemHeight = 12;
+            this.production_listBox.Location = new System.Drawing.Point(400, 382);
+            this.production_listBox.Margin = new System.Windows.Forms.Padding(2);
             this.production_listBox.Name = "production_listBox";
-            this.production_listBox.Size = new System.Drawing.Size(523, 94);
+            this.production_listBox.Size = new System.Drawing.Size(393, 76);
             this.production_listBox.TabIndex = 14;
             // 
             // button5
             // 
-            this.button5.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button5.Location = new System.Drawing.Point(268, 279);
+            this.button5.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.Location = new System.Drawing.Point(400, 230);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(159, 92);
+            this.button5.Size = new System.Drawing.Size(119, 74);
             this.button5.TabIndex = 15;
             this.button5.Tag = "10";
             this.button5.Text = "綠茶($10)";
@@ -239,10 +260,11 @@
             // 
             // button6
             // 
-            this.button6.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button6.Location = new System.Drawing.Point(451, 279);
+            this.button6.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button6.Location = new System.Drawing.Point(537, 230);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(159, 92);
+            this.button6.Size = new System.Drawing.Size(119, 74);
             this.button6.TabIndex = 16;
             this.button6.Tag = "20";
             this.button6.Text = "可樂($20)";
@@ -251,21 +273,33 @@
             // 
             // button7
             // 
-            this.button7.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button7.Location = new System.Drawing.Point(632, 279);
+            this.button7.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button7.Location = new System.Drawing.Point(673, 230);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(159, 92);
+            this.button7.Size = new System.Drawing.Size(119, 74);
             this.button7.TabIndex = 17;
             this.button7.Tag = "40";
             this.button7.Text = "果汁($40)";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(306, 373);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
             // Order_From
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1066, 646);
+            this.ClientSize = new System.Drawing.Size(1018, 506);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -284,11 +318,13 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Order_From";
             this.Text = "Order_From";
             this.Load += new System.EventHandler(this.Order_From_Load);
             this.groupBox.ResumeLayout(false);
             this.groupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -315,5 +351,6 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- WindowsFormsApp1/Order_From.cs
+++ WindowsFormsApp1/Order_From.cs
@@ -23,6 +23,29 @@ namespace WindowsFormsApp1 {
                 comboBox.Items.Add(i);
             }
             comboBox.SelectedIndex = 0;
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add(new DataColumn("product_name"));
+            dt.Columns.Add(new DataColumn("price"));
+
+            string[] name_arr = new string[3] { "奶茶", "紅茶", "綠茶" };
+            string[] price_arr = new string[3] { "50", "30", "10" };
+            
+            for (int i = 0; i < 3; i++)
+            {
+                DataRow rowz = dt.NewRow();
+                rowz["product_name"] = name_arr[i];
+                rowz["price"] = price_arr[i];
+                dt.Rows.Add(rowz);
+            }
+
+            dataGridView1.DataSource = dt;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma", 9.75F, FontStyle.Bold);
+            dataGridView1.Columns["product_name"].HeaderText = "產品名稱";
+            dataGridView1.Columns["price"].HeaderText = "價錢";
+            dataGridView1.DefaultCellStyle.Font = new Font("Tahoma", 8);
+            dataGridView1.AllowUserToAddRows = false;
+
         }
 
         private void button1_Click(object sender, EventArgs e) {
@@ -90,6 +113,29 @@ namespace WindowsFormsApp1 {
 
         private void guest_radioButton_CheckedChanged(object sender, EventArgs e) {
             cal_pay();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView dgv = sender as DataGridView;
+            int total_row = dgv.RowCount;
+
+            if (e.RowIndex >= 0)
+            {
+                string name = dgv.Rows[e.RowIndex].Cells["product_name"].Value.ToString();
+                string price = dgv.Rows[e.RowIndex].Cells["price"].Value.ToString();
+                string result = String.Format("Row: {0}, Column: {1}, Total Row: {2}, Name: {3}, Price: {4}",
+                         e.RowIndex, e.ColumnIndex, total_row, name, price);
+                MessageBox.Show(result);
+
+            }
+            if (e.RowIndex >= 0 && e.ColumnIndex == -1)
+            {
+                string result = String.Format("Row: {0}, Column: {1}, Total Row: {2}, Name: {3}, Price: {4}",
+                         e.RowIndex, e.ColumnIndex, total_row, 1, 1);
+                MessageBox.Show(result);
+                dgv.Rows.RemoveAt(e.RowIndex);
+            }
         }
     }
 }
